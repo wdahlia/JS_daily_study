7175fb145e92ad6615b2e1219e7cb786a4f57b44
HYE <jelly12paw@gmail.com>
2023-02-02 22:03:33 +0900

    Day13 Text To Speech Converter

---
+++ 박혜진/Day13_Text To Speech Converter/day13/main.js
@@ -0,0 +1,34 @@
+let text = document.getElementById('txt');
+let submitBtn = document.getElementById('submit');
+let resumeBtn = document.getElementById('resume');
+let pauseBtn = document.getElementById('pause');
+let audioMessage;
+
+submitBtn.addEventListener('click', () => {
+    audioMessage.text = text.value;
+    window.speechSynthesis.speak(audioMessage);
+});
+
+resumeBtn.addEventListener('click', () => {
+    pauseBtn.style.display = 'block';
+    resumeBtn.style.display = 'none';
+
+    if (speechSynthesis.pause) {
+        speechSynthesis.resume();
+    }
+});
+
+pauseBtn.addEventListener('click', () => {
+    pauseBtn.style.display = 'none';
+    resumeBtn.style.display = 'block';
+
+    speechSynthesis.speaking ? speechSynthesis.pause() : "";
+});
+
+window.onload = () => {
+    if ('speechSynthesis' in window) {
+        audioMessage = new SpeechSynthesisUtterance();
+    } else {
+        alert('Speech Synthese is not supported');
+    }
+}
