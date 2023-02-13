a7a00d7cef7c646e8182e997d15e667c326b3f21
avocad06 <miggo2704@gmail.com>
2023-02-13 21:52:13 +0900

    add: userList추가

---
+++ 안예지/0213/quick-start/src/components/Profile.js
@@ -0,0 +1,15 @@
+const userList = [{
+    id: 1,
+    name: '류진숙',
+    imageUrl: 'https://dummyimage.com/90/bdaaeb/fffce3&text=dahila',
+},
+{
+    id: 2,
+    name: '박혜진',
+    imageUrl: 'https://dummyimage.com/90/eba7b3/fffce3&text=hye',
+},
+{
+    id: 3,
+    name: '안예지',
+    imageUrl: 'https://dummyimage.com/90/b7ebab/fffce3&text=avocado',
+},]
